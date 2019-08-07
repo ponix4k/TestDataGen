77a69c9075bd38986ee52d191f4703782b7d549f
ponix4k <ponix4k@gmail.com>
2019-08-07 21:39:55 +0100

    Created TestDataToCSV.py

--- TestData.py
+++ TestData.py
@@ -1,8 +1,7 @@
 #! /usr/bin/python3
 import sqlite3
-#import mysql.connector
-#from mysql.connector import errorcode
 import random
+
 from faker import Faker
 TD = Faker('en_GB')#this can be set to other languages see docs for more info
 connection = sqlite3.connect('TestData.db')
@@ -11,7 +10,8 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS customers (name text, address text, email text)''')
 
 cursor.execute('INSERT INTO customers VALUES(:name, :address, :email)',
-               {'name': TD.name(), 'address': TD.address(), 'email': TD.email()}
+               {'name': TD.name(), 'address': TD.address(), 'email': TD.email()
+                }
                )
 
 cursor.execute('SELECT * FROM customers')
@@ -22,21 +22,3 @@ for i in cursor.execute('SELECT name, address, email FROM customers'):
 connection.commit()
 
 connection.close()
-
-#var1 = input("How Many Identities should be generated? : ")
-#count = (int(var1))
-#class Customers:
-#        def __init__(self, name, address, email):
-#            self.name = name
-#            self.age = random.randint(18,65)
-#            self.address = address
-#            self.email = email
-#        def __repr__(self):
-#            return 'Name: {}Age: {}Address: {},Email: {},'.format(self.name,self.age,self.address,self.email)
-
-#customer1 = Customers(TD.name(),TD.address(),TD.email())
-#for _ in range(0,count):
-#    print("##########  START  ##########")
-#    print(customer1)
-#    print("##########   END  ###########")
-#    print("")
---
+++ TestDataToCSV.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+import csv
+import sqlite3
+import random
+from faker import Faker
+TD = Faker('en_GB')
+
+count =int(input('How many ID should be generated? : '))  
+
+def create_test_data(amount):
+    data = []
+    for _ in range(0,amount):
+        data.append(TD.name())
+        data.append(TD.address())
+    return data
+
+with open ('test_data.csv', mode='w', newline='') as csvfile:
+    fieldnames = ['name', 'address']
+    csv_test_data = csv.writer(csvfile, delimiter=',', quotechar='"')
+    for i in create_test_data(count):
+        csv_test_data.writerow([i])
