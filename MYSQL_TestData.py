8dfeae8d0bca615a8491471cdd27c2e1de3bcc12
ponix4k <ponix4k@gmail.com>
2019-08-14 22:28:08 +0100

    Created MYSQL connection and files to configure to your own MYSQL instance (Connection.conf)

--- MYSQL_TestData.py
+++ MYSQL_TestData.py
@@ -2,23 +2,13 @@
 import random
 import mysql.connector
 from mysql.connector import errorcode
-
-try:
-    mydb = mysql.connector.connect(host='localhost',user='store',
-                               password='store',database='store')
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-else:
-    mydb.close()
-
-mycursor = mydb.cursor()
-
-mycursor.execute("select * from customers")
-
 from faker import Faker
 TD = Faker('en_GB')#this can be set to other languages see docs for more info
+
+mydb = mysql.connector.connect(option_files='Connection.conf')
+print("Succsess Conection ID:", mydb.connection_id)
+mycursor = mydb.cursor()
+mycursor.execute("select * from Customers")
+
+
+mydb.close()
---
+++ create_store_user.sql
@@ -0,0 +1,2 @@
+CREATE USER 'store'@'localhost' IDENTIFIED BY 'store';
+GRANT ALL PRIVILEGES ON store. * TO 'store'@'localhost';
