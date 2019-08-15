5c50b42cd9f806ee1649e593e0cce9dcbafb9978
ponix4k <ponix4k@gmail.com>
2019-08-15 15:28:12 +0100

    .

--- MYSQL_TestData.py
+++ MYSQL_TestData.py
@@ -8,21 +8,5 @@ TD = Faker('en_GB')#this can be set to other languages see docs for more info
 mydb = mysql.connector.connect(option_files='Connection.conf')
 print("Succsess Conection ID:", mydb.connection_id)
 mycursor = mydb.cursor()
-
-Sql1 = """
-INSERT INTO `Customers`(`Name`, `Email`, `Age`, `Phone`) 
-VALUES ("""+TD.firstname()+""","""+TD.email()+""",18,"""+TD.phone_number()+""")
-"""
-mycursor.execute(Sql1)
-
-Sql2 = """
-select * from Customers
-"""
-mycursor.execute(Sql2)
-
-#iterate over resultss
-for row in mycursor:
-   print(row)
-
-mycursor.close()
+mycursor.execute("Show tables")
 mydb.close()
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!
 
 sudo apt-get install python-pip -y
+sudo apt-get install mariadb-server-10.0 -y
+sudo apt-get install mariadb-server mariadb-client -y
 sudo pip3 install -r requirements.txt
 
