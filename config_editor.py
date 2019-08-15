91977c5e7a4cb0fad6536758546df203a23e780a
ponix4k <ponix4k@gmail.com>
2019-08-15 12:50:11 +0100

    initall commit

---
+++ config_editor.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+Server = (input('Enter Servername to use: '))
+Port =(input('Enter port (default is 3306): '))
+Database =(input('Enter database name to use: '))
+User = (input('Enter Username you wish to login with: '))
+Password = (input('Enter Password to use for login: '))
+print('###############################')
+print('#######|SERVER CONFIG|#########')
+print('###############################')
+print('   Server       : ',Server,'  ')
+print('   PortNo       : ',Port,'     ')
+print('   Database Name: ',Database,'    ')
+print('   Username     : ',User,'  ')
+print('###############################')
+
