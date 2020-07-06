6eabde9a918a0f93d1e1ddee0533cd209688ae1d
John Williams <2320804+ponix4k@users.noreply.github.com>
2020-07-06 23:57:34 +0100

    main file rework
    rewrote the main file to have auto incrementing id.
    added the parts functionality

--- SQL3.py
+++ SQL3.py
@@ -35,172 +35,114 @@ class Person:
     
     def email(self):
         return '{}.{}@{}'.format(self.first_name, self.last_name, self.domain_name)
-        print(self.Email)
-
+        
     def DOB(self):
         return '{}'.format(self.birthDate)
 
-def create_users(first_name,last_name,DOB,email,password,StartDate,Job_Title):
-    dbinit()
-    person = Person(TD.first_name(),TD.last_name(),TD.domain_name(),TD.date_of_birth(minimum_age=17, maximum_age=85))
+def create_users(first_name,last_name,BirthDate,email,password,Job_Title,StartDate):
     conn = sqlite3.connect("TestData.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO Users (First_Name,Last_Name,Birthdate,Email,Password,job_Title,StartDate) Values (?,?,?,?,?,?,?)",
-    (person.first_name,person.last_name,person.DOB(),person.email(),create_password(16),Job_Title,StartDate))
+    cur.execute("INSERT INTO Users (First_Name,Last_Name,Birthdate,Email,Password,Job_Title,StartDate) Values (?,?,?,?,?,?,?)",
+    (first_name,last_name,BirthDate,email,password,Job_Title,StartDate))
+    conn.commit()
+    print ("Data Added")
+    conn.close()
+
+def multi_create_users():
+    i = 0 
+    count = input("How many names would you like to generate?: ")
+    while i < int(count):
+        create_users(TD.first_name(),TD.last_name(),str(TD.date_of_birth(minimum_age=17, maximum_age=85)),TD.email(),create_password(16),TD.job(),str(TD.date_this_century()))
+        i += 1
+
+def select_users():
+    conn=sqlite3.connect("TestData.db")
+    cur=conn.cursor()
+    cur.execute('SELECT * FROM Users')
+    for i in cur.execute('SELECT UID,First_Name, Last_Name, Birthdate, Password, Email, Job_Title, StartDate FROM Users'):
+        print(i)
+    conn.close()
+
+#create_users(TD.first_name(),TD.last_name(),str(TD.date_of_birth(minimum_age=17, maximum_age=85)),email,create_password(16),TD.job(),str(TD.date_this_century()))
+
+def create_part(Part_Name,Cost,BaseCurrency,QTY):
+    conn = sqlite3.connect('TestData.db')
+    cur=conn.cursor()
+    cur.execute("INSERT INTO PARTS (Part_Name,Cost,BaseCurrency,QTY) Values (?,?,?,?)",(Part_Name,Cost,BaseCurrency,QTY))
     conn.commit()
     conn.close()
 
-#user = Person(TD.first_name(),TD.last_name(),TD.domain_name(),TD.date_of_birth(minimum_age=17, maximum_age=85))
+def multi_create_parts():
+    i = 0 
+    count = input("How many names would you like to generate?: ")
+    while i < int(count):
+        create_part(TD.word()+'-'+str(TD.random_int(1,10000)),TD.random_int(1,100),'£',TD.random_int(1,100))
+        i += 1
 
-#print (user.first_name,user.last_name,user.email(),user.DOB())
+def select_parts():
+    conn=sqlite3.connect("TestData.db")
+    cur=conn.cursor()
+    cur.execute("select * from PARTS")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
 
-create_users(Person(TD.first_name(),TD.last_name(),TD.domain_name(),TD.date_of_birth(minimum_age=17, maximum_age=85)),TD.job(),TD.date_this_century())
+def main():
+    opt1 = input("Do you want to READ or WRITE (R/W/eXit): ")
+    if opt1.upper() == 'R':
+        opt1a = input("What data would you like to read ? Customers ,User ,All (C,U,A): ")
+        if opt1a.upper() == 'C':
+            print("select customers")
+            #select_customers()
+        elif opt1a.upper() == 'U':
+            select_users()
+        elif opt1a.upper() == 'P':
+            select_parts()
+        elif opt1a.upper() == 'A':
+            select_all_data()
+        else:
+            print ("Invalid selection")
+    elif opt1.upper() == 'W':
+            opt2 = input("What data would you like to Create ? Customers ,Users, Parts (C,U,P): ")
+            if opt2.upper() == 'C':
+                    opt2a = input("Do you want to create more than one record? (Y/N/eXit): ")
+                    if opt2a.upper() == 'Y':
+                        #multi_create_users()
+                        pass
+                    elif opt2a.upper() == 'N':
+                        #create_customers()
+                        pass
+                    elif opt2a.upper() == 'X':
+                        print("Goodbye")
+                        exit()
 
-#def multi_create_users():
- #   i = 0 
- #   count = input("How many names would you like to generate?: ")
- #   while i < int(count):
- #       create_users()
- #       i += 1
+            elif opt2.upper() == 'U':
+                    opt2a = input("Do you want to create more than one record? (Y/N/eXit): ")
+                    if opt2a.upper() == 'Y':
+                        multi_create_users()
+                    elif opt2a.upper() == 'N':
+                        create_users(TD.first_name(),TD.last_name(),str(TD.date_of_birth(minimum_age=17, maximum_age=85)),TD.email(),create_password(16),TD.job(),str(TD.date_this_century()))
+                    elif opt2a.upper() == 'X':
+                        print("Goodbye")
+                        exit()
+            elif opt2.upper() == 'P':
+                    opt2a = input("Do you want to create more than one record? (Y/N/eXit): ")
+                    if opt2a.upper() == 'Y':
+                        multi_create_parts()
+                    elif opt2a.upper() == 'N':
+                        create_part()
+                    elif opt2a.upper() == 'X':
+                        print("Goodbye")
+                        exit()
+            else:
+                print ("Invalid selection")
 
-#def create_customers():
-#    connection = sqlite3.connect('TestData.db')
-#    cursor = connection.cursor()
-#    cursor.execute('CREATE TABLE IF NOT EXISTS CUSTOMERS ("CID" INTEGER,"Name" TEXT, "Birthdate" TEXT, "Address" TEXT, "Email" TEXT);')
-#    cursor.execute('INSERT INTO CUSTOMERS VALUES(:CID, :Name, :Birthdate, :Address, :Email)',{'CID':TD.random_int(1,100), 'Name': TD.name(), 'Birthdate': str(TD.date_of_birth(minimum_age=17, maximum_age=85))
-#    ,'Address': TD.address(), 'Email': TD.email()})
-#    select_customers()
-#    connection.commit()
-#    connection.close()
+    elif opt1.upper() == 'X':
+        print("Goodbye")
+        exit()
+    else:
+        print("Invalid option please try again")
+    main()
 
-#def multi_create_customers():
-#    i = 0 
-#    count = input("How many names would you like to generate?: ")
-#    while i < int(count):
-#        create_customers()
-#        i += 1
-
-#def create_part():
-#    connection = sqlite3.connect('TestData.db')
-#    cursor = connection.cursor()
-#    partName = (TD.word()+'-'+str(TD.random_int(1,10000)))
-#    cursor.execute('CREATE TABLE IF NOT EXISTS PARTS ("PID" TEXT, "Part_Name" TEXT, "Cost" real,"BaseCurrency" TEXT, "QTY" INTEGER);')
-#    cursor.execute('INSERT INTO PARTS VALUES(:PID, :Part_Name, :Cost, :BaseCurrency, :QTY)',{'PID':TD.random_int(1,100), 'Part_Name': partName,'BaseCurrency':'£', 'Cost':TD.random_int(1,100)
-#    ,'QTY': TD.random_int(1,100)})
-#    connection.commit()
-#    connection.close()
-
-#def multi_create_parts():
-#    i = 0 
-#    count = input("How many parts would you like to generate?: ")
-#    while i < int(count):
-#        create_part()
-#        i += 1
-        
-
-#def select_users():
-#    connection = sqlite3.connect('TestData.db')
-#    cursor = connection.cursor()
-#    print ("############################")
-#    print ("########### Users ##########")
-#    print ("############################")
-#    print("Name, Dob, Password, Email, job title, start date")
-#    cursor.execute('SELECT * FROM Users')
-#    for i in cursor.execute('SELECT UID,First_Name, Last_Name, Birthdate, Password, Email, Job_Title, StartDate FROM Users'):
-#        print(i)
-#    connection.close()
-#    print ("############################")#
-
-#def select_customers():
-#    connection = sqlite3.connect('TestData.db')
-#    cursor = connection.cursor()
-#    print ("############################")
-#    print ("######## Customers #########")
-#    print ("############################")
-#    cursor.execute('SELECT * FROM CUSTOMERS')
-#    for i in cursor.execute('SELECT CID, Name, Birthdate, Address, Email FROM CUSTOMERS'):
-#        print(i)
-#    connection.close()
-#    print ("#########################")
-
-#def select_parts():
-#    connection = sqlite3.connect('TestData.db')
-#    cursor = connection.cursor()
-#    print ("############################")
-#    print ("########## PARTS ###########")
-#    print ("############################")
-#    cursor.execute('SELECT * FROM PARTS')
-#    for i in cursor.execute('SELECT PID, Part_Name, Cost, BaseCurrency, QTY FROM PARTS'):
-#        print(i)
-#    connection.close()
-#    print ("#########################")
-
-#def select_all_data():
-#    select_customers()
-#    print("\n")
-#    select_users()
-#    print("\n")
-#    select_parts()
-#    print("\n")
-
-#def main():
-#    init()
-
- 
- #   opt1 = input("Do you want to READ or WRITE (R/W/eXit): ")
- #   if opt1.upper() == 'R':
- #       opt1a = input("What data would you like to read ? Customers ,Users ,Parts, All (C,U,P,A): ")
- #       if opt1a.upper() == 'C':
- #           select_customers()
- #       elif opt1a.upper() == 'U':
- #           select_users()
- #       elif opt1a.upper() == 'P':
- #           select_parts()
- #       elif opt1a.upper() == 'A':
- #           select_all_data()
- #       else:
- #           print ("Invalid selection")
- #   elif opt1.upper() == 'W':
- #           opt2 = input("What data would you like to Create ? Customers ,Users, Parts (C,U,P): ")
- #           if opt2.upper() == 'C':
- #                   opt2a = input("Do you want to create more than one record? (Y/N/eXit): ")
- #                   if opt2a.upper() == 'Y':
-  #                      multi_create_customers()
-  #                  elif opt2a.upper() == 'N':
-  #                      create_customers()
-  #                  elif opt2a.upper() == 'X':
-  #                      print("Goodbye")
-  #                      exit()
-
-#            elif opt2.upper() == 'U':
-#                    opt2a = input("Do you want to create more than one record? (Y/N/eXit): ")
-#                    if opt2a.upper() == 'Y':
-#                        multi_create_users()
-#                    elif opt2a.upper() == 'N':
-#                        create_users(firstName,lastName,str(TD.date_of_birth(minimum_age=17, maximum_age=85)),create_password(16),email,TD.job(),StartDate)
-#                    elif opt2a.upper() == 'X':
-#                        print("Goodbye")
-#                        exit()
-#            elif opt2.upper() == 'P':
-#                    opt2a = input("Do you want to create more than one record? (Y/N/eXit): ")
-#                    if opt2a.upper() == 'Y':
-#                        multi_create_parts()
-#                    elif opt2a.upper() == 'N':
-#                        create_part()
-#                    elif opt2a.upper() == 'X':
-#                        print("Goodbye")
-#                        exit()
- #           else:
-  #              print ("Invalid selection")
-#
- #   elif opt1.upper() == 'X':
-  #      print("Goodbye")
-   #     exit()
-   # else:
-   #     print("Invalid option please try again")
-   
-    #main()
-
-#if __name__ == "__main__":
-#    main()
-    
+if __name__ == "__main__":
+    main()
