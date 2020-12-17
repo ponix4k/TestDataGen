ae8084c18f8d035cec8e334261e818379943f821
John Williams <2320804+ponix4k@users.noreply.github.com>
2020-12-17 15:52:30 +0000

    started work on potential gui using tkinger

--- main2.py
+++ main2.py
@@ -1,31 +1,55 @@
 #Importing library
-import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+import tkinter 
 #from PIL import ImageTk,Image
 
-#define functions
-FunctionList = [
-    "Create User",
-    "Create Users"
-]
-app = tk.Tk()
+class Example(Frame):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-app.geometry('600x100')
+    def initUI(self):
+        self.master.title("Test Data Generator")
+        menubar = Menu(self.master)
+        self.master.config(menu=menubar)
+        fileMenu = Menu(menubar)
+        submenu = Menu(fileMenu)
+        submenu.add_command(label="New Database")
+        submenu.add_command(label="Open Database")
+        fileMenu.add_cascade(label='Import',menu=submenu, underline=0)
+        helpMenu = Menu(menubar)
+        submenu = Menu(helpMenu)
+        
+        #fileMenu.add_seperator()
+        
+        fileMenu.add_command(label="Exit",underline = 0,command=self.onExit)
+        menubar.add_cascade(label="File",underline = 0, menu=fileMenu)
+        
+        helpMenu.add_command(label="About",underline = 0, menu = helpMenu)
+        menubar.add_cascade(label="Help",underline = 0, menu = helpMenu)
+    def onExit(self):
+        self.quit()
+    
+    def openAbout():
+        newWindow = Toplevel(app)
+        newWindow.geometry("250x250")
+        newWindow.title("About")
+        newWindow.resizable = (False,False)
+        lblAuth = Label(newWindow, text="John Williams")
+        lblSite = Label(newWindow, text="https://projectmoonrope.co.uk")
+        lblAuth.grid(row=1)
+        lblSite.grid(row=4)
+    
+def main():
+    root = Tk()
+    root.title("Test Data Generator")
+    root.geometry("250x150+300+300")
+    app = Example()
+    root.mainloop()
+    
+if __name__ == '__main__':
+    main()
 
-variable = tk.StringVar(app)
-variable.set(FunctionList[0])
-
-opt = tk.OptionMenu(app, variable, *FunctionList)
-opt.config(width=90, font=('terminal', 12))
-opt.pack(side="top")
-
-#Label
-labelTest = tk.Label(text="",font=('terminal', 12), fg='red')
-labelTest.pack(side="top")
-
-#function
-def callback(*args):
-    labelTest.configure(text="The Function You Have Selected {}".format(variable.get()))
-
-variable.trace("w",callback)
-
-app.mainloop()
+#app.mainloop()
